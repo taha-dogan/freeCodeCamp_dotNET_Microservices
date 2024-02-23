54a3492b1dedbc455b716286b182962397141c75
flakysob <flakysob@gmail.com>
2024-02-23 17:05:04 +0300

    first commit

---
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase //ControllerBase kullanımı daha iyi bir sonuç verir
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Bronze Sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow),
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetById(Guid id)
+        {
+            var item = items.Where(p => p.Id == id).SingleOrDefault();
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault(); //güncellenmesi gereken tekil kaydın varlığı (tüm özellikleri) belirleniyor. [Nereye]
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price,
+            }; //var olan ve güncellenmesi gereken item'in kopyası burada elde ediliyor. [Neyi]
+
+            var index = items.FindIndex(existingItem => existingItem.Id == id); //listedeki güncellenecek öğenin indeksinin bulunması 
+            items[index] = updatedItem; //listede güncellenecek indexteki kayıt atanmakta.
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            
+            if(index < 0)
+            {
+                return NotFound();
+            }
+            items.RemoveAt(index);
+            return NoContent();
+        }
+    }
+}
